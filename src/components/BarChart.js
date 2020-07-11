6d77e7f40a3f44b12cfa31c3627c5ccb4b4b812c
Ali Imran <imran.sheikh@gmail.com>
2020-07-11 07:53:39 -0400

    Updated Bar Chart

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -1,66 +1,40 @@
-import React, {Component} from 'react';
-import { Line } from 'react-chartjs-2';
+import React, { useContext } from "react";
+import { Bar } from "react-chartjs-2";
+import { GlobalContext } from '../context/GlobalContext';
 
-const data = {
-  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-  datasets: [
-    {
-      label: 'Recovered',
-      fill: false,
-      lineTension: 0.1,
-      backgroundColor: 'rgba(75,192,192,0.4)',
-      borderColor: 'rgba(75,192,192,1)',
-      borderCapStyle: 'butt',
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: 'miter',
-      pointBorderColor: 'rgba(75,192,192,1)',
-      pointBackgroundColor: '#fff',
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-      pointHoverBorderColor: 'rgba(220,220,220,1)',
-      pointHoverBorderWidth: 2,
-      pointRadius: 1,
-      pointHitRadius: 10,
-      data: [65, 59, 80, 81, 56, 55, 40]
-    },
-    {
-      label: 'Deaths',
-      fill: false,
-      lineTension: 0.1,
-      backgroundColor: 'rgba(75,192,192,0.4)',
-      borderColor: 'rgba(175,192,192,1)',
-      borderCapStyle: 'butt',
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: 'miter',
-      pointBorderColor: 'rgba(75,192,192,1)',
-      pointBackgroundColor: '#fff',
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-      pointHoverBorderColor: 'rgba(220,220,220,1)',
-      pointHoverBorderWidth: 2,
-      pointRadius: 1,
-      pointHitRadius: 10,
-      data: [165, 159, 180, 181, 156, 155, 140]
-    }
-  ]
-};
+function BarChart() {
+  // Use State
+  const[record,setRecord] = useContext(GlobalContext);
+  // Barchart
+  const Barchart = (
+    <Bar
+      data={{
+        labels: ["Total Cases","Serious", "Recovered", "Deaths"],
+        datasets: [
+          {
+            label: "People",
+            backgroundColor: [
+              "darkgrey",
+              "blue",
+              "green",
+              "red"
+            ],
+            data: [record.total_cases, record.total_serious_cases,record.total_recovered,record.total_deaths],
+          },
+        ],
+      }}
+      options={{
+        legend: { display: false },
+        title: { display: true, text: `Current State` },
+      }}
+    />
+  );
 
-export default class BarChart extends Component {
-  render() {
-    return (
-      <div>
-        <h2>Line Chart</h2>
-        <Line ref="chart" data={data} />
-      </div>
-    );
-  }
-
-  componentDidMount() {
-    const { datasets } = this.refs.chart.chartInstance.data
-    console.log(datasets[0].data);
-  }
+  return (
+    <div >
+      {Barchart}
+    </div>
+  );
 }
+
+export default BarChart;
